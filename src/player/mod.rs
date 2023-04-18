e7ae7e6401c092c7edb82a84e7f38e337b0863e2
utkarsh-dev <utkarsh17ife@gmail.com>
2023-04-18 17:13:14 +0530

    system ordering

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -13,7 +13,7 @@ pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(spawn_player)
-            .add_system(player_movement)
+            .add_system(player_movement.before(confine_player_movement))
             .add_system(confine_player_movement)
             .add_system(enemy_hit_player)
             .add_system(player_hit_star);
